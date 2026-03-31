7b06893ceeb09cc55ee63d849467490c2e12e1a0
oyshik <oyshikf@gmail.com>
2026-03-31 12:33:56 +0600

    chore: add Docker setup and update settings

---
+++ Dockerfile
@@ -0,0 +1,25 @@
+FROM python:3.12-slim
+
+ENV PYTHONDONTWRITEBYTECODE=1
+ENV PYTHONUNBUFFERED=1
+
+WORKDIR /movie_reservation_app
+
+RUN apt-get update && apt-get install -y \
+    gcc \
+    postgresql-client \
+    libpq-dev \
+    && rm -rf /var/lib/apt/lists/*
+
+COPY requirements.txt .
+RUN pip install -r requirements.txt
+
+COPY . .
+
+RUN python manage.py collectstatic --noinput || true
+
+EXPOSE 8000
+
+
+CMD ["gunicorn", "--bind", "0.0.0.0:8000", "movie_reservation.wsgi:application"]
+
--- movie_reservation/settings.py
+++ movie_reservation/settings.py
@@ -86,16 +86,22 @@ TEMPLATES = [
 
 WSGI_APPLICATION = "movie_reservation.wsgi.application"
 
+env = environ.Env()
+environ.Env.read_env()
+
 
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+if env("DATABASE_URL", default=None):
+    DATABASES = {"default": env.db("DATABASE_URL")}
+else:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": BASE_DIR / "db.sqlite3",
+        }
     }
-}
 
 
 # Password validation
@@ -115,9 +121,6 @@ AUTH_PASSWORD_VALIDATORS = [
         "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
     },
 ]
-
-env = environ.Env()
-environ.Env.read_env()
 
 
 # Internationalization
